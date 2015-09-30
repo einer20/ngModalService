40a04238e19652a839638daacacdeee52a1ea8fb
einersantanar <einersantanar@hotmail.com>
2015-09-30 16:59:07 -0400

    Added the dependencies parameters

--- ngModalService.js
+++ ngModalService.js
@@ -1,3 +1,25 @@
 
 
-angular.module("$ngModalService",["$document","$rootScope",""]);
+/*
+	Creates a modal dialog
+*/
+angular.module("",[]).service("ngModalService",["$document", "$rootScope","$controller", "compile", "$http", 
+	function($document, $rootScope, $controller, $compile,$http){
+  
+	var defaultOptions = {
+
+		maximize : false,
+		showFooter : false,
+		title : "Content",
+		css: {},
+	};
+
+  return ngModalService(options){
+
+  	options = angular.extend(defaultOptions, options);
+
+
+
+  }
+
+}]);
