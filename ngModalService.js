89de00163d3f9936bed2f435b97a2f8f1d1231e3
einersantanar <einersantanar@hotmail.com>
2015-09-30 17:16:35 -0400

    Defined template information and events created.

--- ngModalService.js
+++ ngModalService.js
@@ -18,8 +18,87 @@ angular.module("",[]).service("ngModalService",["$document", "$rootScope","$cont
 
   	options = angular.extend(defaultOptions, options);
 
+  	var modalTemplateString = '<div class="ng-modal">' +
+                              '<div class="ng-modal-dialog">' +
+                                '<div class="ng-modal-content">' +
+                                  '<div class="ng-modal-header">' +
+                                    '<button ng-click="modal.close()" type="button" >&times;</button>' +
+                                    '<span class="ng-modal-icon" id="ng-modal-icon"></span>' +
+                                    '<h4 class="ng-modal-title"></h4>' +
+                                  '</div>' +
+                                  '<div class="ng-modal-body">' +
+                                    // content here
+                                   '</div>' +
+                                   //'<div style="display:block;z-index:-1"> </div>' +
+                                    '<div class="ng-modal-footer">' +
+                                     '<div class="ng-modal-footer-wrapper">' +
+                                      '<button type="button" ng-click="modal.cancelPressed()"></button>' +
+                                      '<button type="button" ng-click="modal.acceptPressed()"></button>' +
+                                    '</div>' +
+                                    '</div>' +
+                                   '</div>' +
+                                '</div>' +
+                            '</div>';
 
+
+     var $template = angular.element(modalTemplateString);
+     var scope = $rootScope.$new();
+
+     // appling the configuration to the modal
+     $template.find(".ng-modal-title").text(options.title);
+
+     if(!options.showFooter)
+     		$template.find(".ng-modal-footer").hide();
+
+
+    // creating the default events
+     scope.modal = {};
+
+     if(!options.cancelPressed)
+     {
+	     // executes when the user press the cancel buttom
+	     scope.modal.cancelPressed = function(){
+
+	     };
+     }
+
+     if(!options.acceptPressed)
+     {
+	     // executes when the user press the accept buttom
+	     scope.modal.acceptPressed = function(){
+
+	     };
+     }
+
+     // destroy the modal from the DOM
+     scope.modal.destroy = function(){
+
+     };
+
+     scope.modal.maximize = function(){
+
+
+     };
+
+     if(options.templateUrl){
+
+
+
+
+     }
+     else if (options.template) {
+     	$template.find("ng-modal-body").html(options.template);
+     }
+
+
+
+
+
+     if (options.getModal) {
+     	options.getModal($template,scope);
+     }
 
   }
 
 }]);
+
